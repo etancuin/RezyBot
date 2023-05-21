4ee8ecb2a186abd9005d86c0409e1258e60f9b0e
etancuin <114794458+etancuin@users.noreply.github.com>
2023-05-21 13:29:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -21,12 +21,5 @@ else if content[0] > 2:
   --guest
   browser.find_element_by_class_name('tock-icon icon_minus').click()
 # =============================
-# Filling in the Facebook Credentials
-browser.find_element_by_name("email").send_keys(fb_name)
-browser.find_element_by_name("pass").send_keys(fb_pass)
-# Click on the facebook log-in button
-browser.find_element_by_id('loginbutton').click();
-# =============================
-# Notification Box pop up => Click on "Not Now"
-browser.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]').click();
-# ... Continue ...
+# Filling in the credentials
+
